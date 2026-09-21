de90f49cf033f9bc2bf7cd6610b39ce2f2e6ff77
Bhuvanesh Neve <bhuvineve05@gmail.com>
2026-09-21 13:38:15 +0000

    Sep 21, 2026, 7:08 PM

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,5 +1,5 @@
-import { ArrowDown, ArrowRight } from "lucide-react";
-
+import Image from "next/image";
+import { ArrowDown, ArrowRight, Code2, Terminal } from "lucide-react";
 export default function Home() {
   return (
     <section
