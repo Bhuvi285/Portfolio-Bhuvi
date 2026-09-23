f0da3a37d6c0ab7ccc5549a22838ade2e74e1bf8
Bhuvanesh Neve <bhuvineve05@gmail.com>
2026-09-23 10:13:47 +0000

    Sep 23, 2026, 3:43 PM

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -104,7 +104,7 @@ const projects = [
     technologies: ["React", "JavaScript"],
     github:
       "https://github.com/Bhuvi285/To-Do-List-with-React-JS",
-    live: "",
+    live: "https://to-do-list-with-react-js-ruddy.vercel.app/",
     status: "completed",
     featured: false,
   },
